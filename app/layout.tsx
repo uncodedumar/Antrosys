c4a8ca90d36bdd77b52fcb96f55172177233a6ff
Muhammad Umar Riaz <m.umarriaz1@outlook.com>
2026-03-03 17:59:14 +0500

    robot.ts creation and favicon.ico type updated to x-icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -119,7 +119,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
-      { url: "/favicon.ico", type: "image/svg+xml" },
+      { url: "/favicon.ico", rel:"icon", type: "image/x-icon" },
     ],
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
