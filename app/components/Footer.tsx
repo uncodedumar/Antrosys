4def283e7b9ca3eee60e4c6283957185ed02b4ea
Saad-AliKhan <74400540+Saad-AliKhan@users.noreply.github.com>
2026-05-12 00:28:28 +0500

    footer fix

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -20,7 +20,7 @@ const Footer = () => {
     "GO DEEPER": [
       { name: "About", href: "/about" }
       { name: "Get in Touch", href: "/contact" },
-      { name: "Book a Meeting", href: "https:/cal.com/antrosys" },
+      { name: "Book a Meeting", href: "https://cal.com/antrosys" },
     ],
     SOCIALS: [
       { name: "Linkedin", href: "https://www.linkedin.com/company/antrosys" },
